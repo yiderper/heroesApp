e8374d9ef7de090a4e056c25b8c4a748203af612
Yider Perlaza <yider.per@gmail.com>
2024-03-18 22:37:21 -0500

    Primer commit

---
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-page',
+  templateUrl: './list-page.component.html'
+})
+export class ListPageComponent {
+
+}
---
+++ src/app/heroes/pages/search-page/search-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-page',
+  templateUrl: './search-page.component.html'
+})
+export class SearchPageComponent {
+
+}
